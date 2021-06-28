d3e21365e139813f89221f3245c1de8f26eb830a
andreagavardi <andreagavardi89@gmail.com>
2021-06-28 15:19:36 +0200

    creato classe Product e classe Laptop con extends

--- index.php
+++ index.php
@@ -1,1 +1,39 @@
 <?php
+class Product
+{
+    protected $name;
+    protected $price;
+    protected $category;
+    private $quantity;
+
+    public function __construct($name, $price, $category, $quantity)
+    {
+        $this->name = $name;
+        $this->price = $price;
+        $this->category = $category;
+        $this->quantity = $quantity;
+    }
+}
+
+class Laptop extends Product
+{
+
+    private $barcode;
+    protected $ram;
+    protected $processor;
+    protected $display;
+    public function _construct($name, $price, $category, $quantity, $barcode, $ram, $processor, $display)
+    {
+        parent::__construct($name, $price, $category, $quantity);
+        $this->barcode = $barcode;
+        $this->ram = $ram;
+        $this->processor = $processor;
+        $this->display = $display;
+    }
+}
+
+$phone = new Product('I-phone', 1500, 'mobile', 25);
+
+$lenovo = new Laptop('Ideapad-Gaming', 1150, 'notebook', 20, 8017596102500, '16 Gb', 'Intel-i7', '14.5 Pollici');
+
+var_dump($phone, $lenovo);
